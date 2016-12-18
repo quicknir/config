51be245115b699cbbc76d8f131592449f8c13d8e
Nir Friedman <quicknir@gmail.com>
2016-12-17 22:08:07 -0500

    Remove layer path that was automatically included

--- spacemacs/spacemacs.d/init.el
+++ spacemacs/spacemacs.d/init.el
@@ -27,7 +27,7 @@ values."
    ;; If non-nil layers with lazy install support are lazy installed.
    ;; List of additional paths where to look for configuration layers.
    ;; Paths must have a trailing slash (i.e. `~/.mycontribs/')
-   dotspacemacs-configuration-layer-path '("~/.spacemacs.d/layers/")
+   dotspacemacs-configuration-layer-path '()
    ;; List of configuration layers to load.
    dotspacemacs-configuration-layers
    '(
