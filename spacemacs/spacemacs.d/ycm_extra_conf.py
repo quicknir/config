ef38bd70a274c1b65cb9c26f47ef02f1536b5ca8
nir friedman <nir@tower-research.com>
2016-12-17 23:37:24 -0500

    Revamp ycm configuration file
     - clean up indentation (2 spaces, yuk)
     - change handling of system includes, correct for gcc toolchains
     - clean up flags, change naming in some places
    
    Change-Id: I98120669d87f3c39ab377a7b1ef2d4d54f5d33a7

--- spacemacs/spacemacs.d/ycm_extra_conf.py
+++ spacemacs/spacemacs.d/ycm_extra_conf.py
@@ -35,59 +35,16 @@ import subprocess
 
 # These are the compilation flags that will be used in case there's no
 # compilation database set (by default, one is not set).
-# CHANGE THIS LIST OF FLAGS. YES, THIS IS THE DROID YOU HAVE BEEN LOOKING FOR.
-extra_flags = ['-Wall', '-Wextra', '-fexceptions', '-std=c++11', '-Wno-unused-parameter',
-               '-fspell-checking']
-
 flags = [
-'-Wall',
-'-Wextra',
-'-Werror',
-'-Wc++98-compat',
-'-Wno-long-long',
-'-Wno-variadic-macros',
-'-fexceptions',
-'-DNDEBUG',
-# You 100% do NOT need -DUSE_CLANG_COMPLETER in your flags; only the YCM
-# source code needs it.
-'-DUSE_CLANG_COMPLETER',
-# THIS IS IMPORTANT! Without a "-std=<something>" flag, clang won't know which
-# language to use when compiling headers. So it will guess. Badly. So C++
-# headers will be compiled as C headers. You don't want that so ALWAYS specify
-# a "-std=<something>".
-# For a C project, you would set this to something like 'c99' instead of
-# 'c++11'.
-'-std=c++11',
-# ...and the same thing goes for the magic -x option which specifies the
-# language that the files to be compiled are written in. This is mostly
-# relevant for c++ headers.
-# For a C project, you would set this to 'c' instead of 'c++'.
-'-x',
-'c++',
-'-isystem',
-'../BoostParts',
-'-isystem',
-# This path will only work on OS X, but extra paths that don't exist are not
-# harmful
-'/System/Library/Frameworks/Python.framework/Headers',
-'-isystem',
-'../llvm/include',
-'-isystem',
-'../llvm/tools/clang/include',
-'-I',
-'.',
-'-I',
-'./ClangCompleter',
-'-isystem',
-'./tests/gmock/gtest',
-'-isystem',
-'./tests/gmock/gtest/include',
-'-isystem',
-'./tests/gmock',
-'-isystem',
-'./tests/gmock/include',
+    '-fexceptions', '-DNDEBUG', '-std=c++14', '-x', 'c++',
+    '-Wno-unused-parameter', '-I', '.', '-I', '../', '-I', '../../'
 ]
 
+# Flags that get added whether or not there is a compilation database
+# Always good to get warnings, and fspellchecking is necessary to get
+# good fixit suggestions.
+extra_flags = ['-Wall', '-Wextra', '-Wno-unused-parameter', '-fspell-checking',
+               '-Wpedantic']
 
 # Set this to the absolute path to the folder (NOT the file!) containing the
 # compile_commands.json file to use that instead of 'flags'. See here for
@@ -101,126 +58,143 @@ flags = [
 # 'flags' list of compilation flags. Notice that YCM itself uses that approach.
 compilation_database_folder = ''
 
-if os.path.exists( compilation_database_folder ):
-  database = ycm_core.CompilationDatabase( compilation_database_folder )
+if os.path.exists(compilation_database_folder):
+    database = ycm_core.CompilationDatabase(compilation_database_folder)
 else:
-  database = None
+    database = None
 
-SOURCE_EXTENSIONS = [ '.cpp', '.cxx', '.cc', '.c', '.m', '.mm' ]
+SOURCE_EXTENSIONS = ['.x.cpp', '.cpp', '.cxx', '.cc', '.c', '.m', '.mm',
+                     '.t.cpp']
 
-def LoadSystemIncludes():
-    regex = re.compile(ur'(?:\#include \<...\> search starts here\:)(?P<list>.*?)(?:End of search list)', re.DOTALL);
-    process = subprocess.Popen(['clang', '-v', '-E', '-x', 'c++', '-'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE);
-    process_out, process_err = process.communicate('');
-    output = process_out + process_err;
-    includes = [];
+system_include_cache = {}
+
+
+def load_system_includes(gcc_toolchain=None):
+    # Typically we'll get the same system includes over and over,
+    # so let's cache the solution
+    if gcc_toolchain in system_include_cache:
+        return system_include_cache[gcc_toolchain]
+
+    if gcc_toolchain is None:
+        gcc = []
+    else:
+        gcc = [gcc_toolchain]
+
+    regex = re.compile(
+        ur'(?:\#include \<...\> search starts here\:)(?P<list>.*?)(?:End of search list)',
+        re.DOTALL)
+    process = subprocess.Popen(
+        ['clang', '-v', '-E', '-x', 'c++', '-'] + gcc,
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE)
+    process_out, process_err = process.communicate('')
+    output = process_out + process_err
+    includes = []
     for p in re.search(regex, output).group('list').split('\n'):
-        p = p.strip();
+        p = p.strip()
         if len(p) > 0 and p.find('(framework directory)') < 0:
-            includes.append('-isystem');
-            includes.append(p);
-    return includes;
+            includes.append('-isystem')
+            includes.append(p)
 
-systemIncludes = LoadSystemIncludes();
+    system_include_cache[gcc_toolchain] = includes
+    return includes
+
 
 def DirectoryOfThisScript():
-  return os.path.dirname( os.path.abspath( __file__ ) )
+    return os.path.dirname(os.path.abspath(__file__))
 
 
-def MakeRelativePathsInFlagsAbsolute( flags, working_directory ):
-  if not working_directory:
-    return list( flags )
-  new_flags = []
-  make_next_absolute = False
-  path_flags = [ '-isystem', '-I', '-iquote', '--sysroot=' ]
-  for flag in flags:
-    new_flag = flag
+def MakeRelativePathsInFlagsAbsolute(flags, working_directory):
+    if not working_directory:
+        return list(flags)
+    new_flags = []
+    make_next_absolute = False
+    path_flags = ['-isystem', '-I', '-iquote', '--sysroot=']
+    for flag in flags:
+        new_flag = flag
 
-    if make_next_absolute:
-      make_next_absolute = False
-      if not flag.startswith( '/' ):
-        new_flag = os.path.join( working_directory, flag )
+        if make_next_absolute:
+            make_next_absolute = False
+            if not flag.startswith('/'):
+                new_flag = os.path.join(working_directory, flag)
 
-    for path_flag in path_flags:
-      if flag == path_flag:
-        make_next_absolute = True
-        break
+        for path_flag in path_flags:
+            if flag == path_flag:
+                make_next_absolute = True
+                break
 
-      if flag.startswith( path_flag ):
-        path = flag[ len( path_flag ): ]
-        new_flag = path_flag + os.path.join( working_directory, path )
-        break
+            if flag.startswith(path_flag):
+                path = flag[len(path_flag):]
+                new_flag = path_flag + os.path.join(working_directory, path)
+                break
 
-    if new_flag:
-      new_flags.append( new_flag )
-  return new_flags
-
-
-def IsHeaderFile( filename ):
-  extension = os.path.splitext( filename )[ 1 ]
-  return extension in [ '.h', '.hxx', '.hpp', '.hh' ]
+        if new_flag:
+            new_flags.append(new_flag)
+    return new_flags
 
 
-def GetCompilationInfoForFile( filename ):
-  # The compilation_commands.json file generated by CMake does not have entries
-  # for header files. So we do our best by asking the db for flags for a
-  # corresponding source file, if any. If one exists, the flags for that file
-  # should be good enough.
-  if IsHeaderFile( filename ):
-    basename = os.path.splitext( filename )[ 0 ]
+def is_header_file(filename):
+    extension = os.path.splitext(filename)[1]
+    return extension in {'.h', '.hxx', '.hpp', '.hh'}
 
+
+def GetCompilationInfoForFile(filename):
+    if not is_header_file:
+        return database.GetCompilationInfoForFile(filename)
+
+    # Header files are not compilation targets, so we need to use heuristics
+    # to get a reasonable set of flags
+
+    # First attempt: find a corresponding implementation or test file
+    basename = os.path.splitext(filename)[0]
     for extension in SOURCE_EXTENSIONS:
-      replacement_file = basename + extension
-      if os.path.exists( replacement_file ):
-        compilation_info = database.GetCompilationInfoForFile(
-          replacement_file )
-        if compilation_info.compiler_flags_:
-          return compilation_info
+        replacement_file = basename + extension
+        if os.path.exists(replacement_file):
+            compilation_info = database.GetCompilationInfoForFile(
+                replacement_file)
+            if compilation_info.compiler_flags_:
+                return compilation_info
 
-    # If can't find alternate extension, just try another file in same directory
-
+    # Second attempt: any file in same directory
     dir = os.path.dirname(filename)
 
     for f in os.listdir(dir):
-      if any(f.endswith(i) for i in SOURCE_EXTENSIONS):
-        compilation_info = database.GetCompilationInfoForFile(os.path.join(dir, f))
-        if compilation_info.compiler_flags_:
-          return compilation_info
+        if any(f.endswith(i) for i in SOURCE_EXTENSIONS):
+            compilation_info = database.GetCompilationInfoForFile(
+                os.path.join(dir, f))
+            if compilation_info.compiler_flags_:
+                return compilation_info
 
     return None
-  return database.GetCompilationInfoForFile( filename )
 
 
-def FlagsForFile( filename, **kwargs ):
-  if database:
-    # Bear in mind that compilation_info.compiler_flags_ does NOT return a
-    # python list, but a "list-like" StringVec object
-    compilation_info = GetCompilationInfoForFile( filename )
-    if not compilation_info:
-      return None
+def FlagsForFile(filename, **kwargs):
+    if database:
+        # Bear in mind that compilation_info.compiler_flags_ does NOT return a
+        # python list, but a "list-like" StringVec object
+        compilation_info = GetCompilationInfoForFile(filename)
+        if not compilation_info:
+            return None
 
-    final_flags =  MakeRelativePathsInFlagsAbsolute(
-      compilation_info.compiler_flags_,
-      compilation_info.compiler_working_dir_ ) + systemIncludes + extra_flags
+        final_flags = MakeRelativePathsInFlagsAbsolute(
+            compilation_info.compiler_flags_,
+            compilation_info.compiler_working_dir_) + extra_flags
 
-    # ycmd's heuristics are broken unfortunately, and decide that compiling with
-    # clang means that we are compiling c code and not c++ code, leading to
-    # marking every use of try/throw/catch as an error
-    # final_flags[2] = 'c++'
-    final_flags[0] = "clang++"
+        # ycmd's heuristics are broken unfortunately, and decide that compiling
+        # with clang means that we are compiling c code and not c++ code,
+        # leading to marking every use of try/throw/catch as an error
+        # final_flags[2] = 'c++'
+        final_flags[0] = "clang++"
 
-    # NOTE: This is just for YouCompleteMe; it's highly likely that your project
-    # does NOT need to remove the stdlib flag. DO NOT USE THIS IN YOUR
-    # ycm_extra_conf IF YOU'RE NOT 100% SURE YOU NEED IT.
-    # try:
-    #   final_flags.remove( '-stdlib=libc++' )
-    # except ValueError:
-    #   pass
-  else:
-    relative_to = DirectoryOfThisScript()
-    final_flags = MakeRelativePathsInFlagsAbsolute( flags, relative_to )
+        # To get system includes, see if gcc toolchain option specified
+        final_flags = final_flags + load_system_includes(
+            next((x for x in final_flags
+                  if x.startswith("--gcc-toolchain")), None))
 
-  return {
-    'flags': final_flags,
-    'do_cache': True
-  }
+    else:
+        relative_to = DirectoryOfThisScript()
+        final_flags = MakeRelativePathsInFlagsAbsolute(
+            flags, relative_to) + extra_flags + load_system_includes()
+
+    return {'flags': final_flags, 'do_cache': True}
