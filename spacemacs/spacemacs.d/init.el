e5dc40292d737d0f4317f7c53fe738ae90ca8141
Nir Friedman <quicknir@gmail.com>
2017-01-05 23:50:43 -0500

    Fix some bugs with vertical cursors

--- spacemacs/spacemacs.d/init.el
+++ spacemacs/spacemacs.d/init.el
@@ -410,37 +410,39 @@ you should place your code here."
   ;; evil mc
   (global-evil-mc-mode 1)
 
+  (defun col-at-point (point)
+    (save-excursion (goto-char point) (current-column)))
+
   (defun evil--mc-make-cursor-at-col-append (_startcol endcol orig-line)
     (end-of-line)
     (when (> endcol (current-column))
       (insert-char ?\s (- endcol (current-column))))
     (move-to-column (- endcol 1))
     (unless (= (line-number-at-pos) orig-line)
-      (message (number-to-string (current-column)))
       (evil-mc-make-cursor-here)))
 
   (defun evil--mc-make-cursor-at-col-insert (startcol _endcol orig-line)
     (end-of-line)
+    (move-to-column startcol)
     (unless (or (= (line-number-at-pos) orig-line) (> startcol (current-column)))
-      (move-to-column startcol)
       (evil-mc-make-cursor-here)))
 
   (defun evil--mc-make-vertical-cursors (beg end func)
     (evil-mc-pause-cursors)
     (apply-on-rectangle func
                         beg end (line-number-at-pos (point)))
-    (evil-mc-resume-cursors)
-    (evil-normal-state)
-    (move-to-column (evil-mc-column-number (min beg end)))
-    )
+    (message (number-to-string (current-column)))(evil-mc-resume-cursors)
+    (evil-normal-state))
 
   (defun evil-mc-insert-vertical-cursors (beg end)
     (interactive (list (region-beginning) (region-end)))
-    (evil--mc-make-vertical-cursors beg end 'evil--mc-make-cursor-at-col-insert))
+    (evil--mc-make-vertical-cursors beg end 'evil--mc-make-cursor-at-col-insert)
+    (move-to-column (min (col-at-point beg) (col-at-point end))))
 
   (defun evil-mc-append-vertical-cursors (beg end)
     (interactive (list (region-beginning) (region-end)))
-    (evil--mc-make-vertical-cursors beg end 'evil--mc-make-cursor-at-col-append))
+    (evil--mc-make-vertical-cursors beg end 'evil--mc-make-cursor-at-col-append)
+    (move-to-column (- (max (col-at-point beg) (col-at-point end)) 1)))
 
   (evil-define-key 'visual global-map "gI" 'evil-mc-insert-vertical-cursors)
   (evil-define-key 'visual global-map "gA" 'evil-mc-append-vertical-cursors)
