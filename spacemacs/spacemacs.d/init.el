4455352693ff2801983577fc9000518c433763f1
nir friedman <nir@tower-research.com>
2017-05-17 09:19:17 -0400

    Set custom file
    Change-Id: Iee3a75a01128b8c3c5e8a25806e1a6fd9b590681

--- spacemacs/spacemacs.d/init.el
+++ spacemacs/spacemacs.d/init.el
@@ -319,6 +319,7 @@ executes.
  This function is mostly useful for variables that need to be set
 before packages are loaded. If you are unsure, you should try in setting them in
 `dotspacemacs/user-config' first."
+  (setq custom-file "./customize.el")
   )
 
 (defun dotspacemacs/user-config ()
