3b980c50751521023ec2c7eaf1da1a795e9ea723
Nir Friedman <quicknir@gmail.com>
2017-06-04 13:15:27 -0400

    Correct location of custom settings file

--- spacemacs/spacemacs.d/init.el
+++ spacemacs/spacemacs.d/init.el
@@ -320,7 +320,7 @@ executes.
  This function is mostly useful for variables that need to be set
 before packages are loaded. If you are unsure, you should try in setting them in
 `dotspacemacs/user-config' first."
-  (setq custom-file "./customize.el")
+  (setq custom-file (concat (file-name-directory load-file-name) "customize.el"))
   )
 
 (defun dotspacemacs/user-config ()
