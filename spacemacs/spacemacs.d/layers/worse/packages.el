32a268270ab88b403e919d69f6b8932b702bbb94
nir friedman <nir@tower-research.com>
2017-06-14 16:16:42 -0400

    Add keybinding for worse package
    Change-Id: I4b18d11319cfc48640f676b113a3392b43ee58ca

--- spacemacs/spacemacs.d/layers/worse/packages.el
+++ spacemacs/spacemacs.d/layers/worse/packages.el
@@ -3,4 +3,5 @@
         (worse :location local)))
 
 (defun worse/init-worse ()
-  (use-package worse))
+  (use-package worse
+    :init (spacemacs/set-leader-keys "pf" 'worse-faster-projectile-files)))
