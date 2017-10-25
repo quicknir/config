6e870ca848398dbab92b7eea48acb0f71396a9f8
Nir Friedman <quicknir@gmail.com>
2017-10-24 23:54:53 -0400

    Add binding for convenient yas use

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -333,6 +333,13 @@ This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
 
+  (require 'yasnippet)
+  (require 'undo-tree)
+  (evil-define-key 'insert undo-tree-map (kbd "C-/") nil)
+  (defun nir-yasnippet-expand-or-complete ()
+    (interactive)
+    (unless (call-interactively 'yas-expand) (call-interactively 'company-yasnippet)))
+  (define-key yas-minor-mode-map (kbd "C-/") 'nir-yasnippet-expand-or-complete)
   ;; Some minor mode customizations
   (electric-pair-mode 1)
   (purpose-mode -1)
