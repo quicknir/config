0ae0fb6c7d584310535adb89dfaf8ed3c04d340f
nir friedman <nir@tower-research.com>
2018-01-04 11:00:41 -0500

    Add gists.el file; place to keep useful but as-yet unused code
    Change-Id: If2a5e7b0676c0ba10c295169f3df2a8007e03d83

---
+++ spacemacs.d/gists.el
@@ -0,0 +1,15 @@
+;; Useful random bits of stuff I want to keep handy. Nicer to keep in a separate
+;; file than commented out in my .spacemacs
+
+  ;; (flycheck-define-checker
+  ;;     python-mypy ""
+  ;;     :command ("mypy"
+  ;;               "--ignore-missing-imports" "--fast-parser"
+  ;;               "--python-version" "3.6"
+  ;;               source-original)
+  ;;     :error-patterns
+  ;;     ((error line-start (file-name) ":" line ": error:" (message) line-end))
+  ;;     :modes python-mode)
+
+  ;; (add-to-list 'flycheck-checkers 'python-mypy t)
+  ;; (flycheck-add-next-checker 'python-pylint 'python-mypy t)
