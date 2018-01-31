a72d315115ea432908f9afd88916eefe370c0573
nir friedman <nir@tower-research.com>
2018-01-31 10:35:10 -0500

    Add helm frame gist
    Change-Id: I11c8196ea6b3101d534ff48d6f90b46b1082a5b0

--- spacemacs.d/gists.el
+++ spacemacs.d/gists.el
@@ -13,3 +13,7 @@
 
   ;; (add-to-list 'flycheck-checkers 'python-mypy t)
   ;; (flycheck-add-next-checker 'python-pylint 'python-mypy t)
+
+;; (setq helm-display-function 'helm-display-buffer-in-own-frame
+;;       helm-display-buffer-reuse-frame t
+;;       helm-use-undecorated-frame-option t)
