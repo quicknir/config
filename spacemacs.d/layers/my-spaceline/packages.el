adee00cfdd1225a026a16daf9b4dfd3e9d54ea00
nir friedman <nir@tower-research.com>
2018-02-02 20:38:07 -0500

    Improve spaceline git segment, add ycmd/evil-mc todos
    Change-Id: I8dd4d5907ac102d123267e64d82e57abbc268ad0

--- spacemacs.d/layers/my-spaceline/packages.el
+++ spacemacs.d/layers/my-spaceline/packages.el
@@ -12,19 +12,65 @@
   (require 'spaceline)
   (require 'spaceline-all-the-icons)
 
-  (spaceline-define-segment nir-git
-    "An `all-the-icons' segment to display Added/Removed stats for files under git VC."
-    (cl-destructuring-bind (added removed modified) (spaceline-all-the-icons--git-statistics)
-      (let* ((icons
-              (list
-               (propertize (concat "+" (number-to-string added)) 'face 'success)
-               (propertize (concat "-" (number-to-string removed)) 'face 'error)
-               (propertize (concat "*" (number-to-string modified)) 'face 'warning))))
+  ;; Use this to define evil-mc segment
+  ;; (defun evil-mc-active-mode-line (prefix)
+  ;;   "Get the mode-line text to be displayed when there are active cursors"
+  ;;   (let ((mode-line-text
+  ;;          (concat mode-line-text-prefix
+  ;;                  (when (and (evil-mc-frozen-p)
+  ;;                             evil-mc-mode-line-text-paused)
+  ;;                    "(paused)")
+  ;;                  (format ":%d" (evil-mc-get-cursor-count)))))
+  ;;     ;; mode line text colors
+  ;;     (cond ((and (evil-mc-frozen-p)
+  ;;                 evil-mc-mode-line-text-inverse-colors)
+  ;;            (propertize mode-line-text 'face '(:inverse-video t)))
+  ;;           ;; resumed (unfrozen) cursors
+  ;;           (evil-mc-mode-line-text-cursor-color
+  ;;            (propertize
+  ;;             mode-line-text
+  ;;             'face
+  ;;             '(:inherit cursor :foreground "black" :distant-foreground "white")))
+  ;;           ;; default colors
+  ;;           (t mode-line-text))))
 
-        (propertize
-         (mapconcat 'identity icons " "))))
-    :when (and active
-               (not (equal '(0 0 0) (spaceline-all-the-icons--git-statistics)))))
+  ;; Use this to define ycmd segment (then remove minor mode segment, or at least toggle off by default)
+  ;; (defun ycmd--mode-line-status-text ()
+  ;;   "Get text for the mode line."
+  ;;   (let ((force-semantic
+  ;;          (when ycmd-force-semantic-completion "/s"))
+  ;;         (text (pcase ycmd--last-status-change
+  ;;                 (`parsed "")
+  ;;                 (`parsing "*")
+  ;;                 (`unparsed "?")
+  ;;                 (`stopped "-")
+  ;;                 (`starting ">")
+  ;;                 (`errored "!"))))
+  ;;     (concat " ycmd" force-semantic text)))
+
+  (spaceline-define-segment nir-git
+    (progn
+      (vc-refresh-state)
+      (pcase (vc-state (buffer-file-name))
+        (`edited
+         (cl-destructuring-bind (added removed modified) (spaceline-all-the-icons--git-statistics)
+           (let* ((icons
+                   (list
+                    "|"
+                    (propertize (concat "+" (number-to-string added)) 'face 'success)
+                    (propertize (concat "-" (number-to-string removed)) 'face 'error)
+                    (propertize (concat "*" (number-to-string modified)) 'face 'warning))))
+
+             (propertize
+              (mapconcat 'identity icons " ")))))
+        (`added "added")
+        (_ "shit see what's going on")
+        ))
+
+    :when (and active vc-mode (buffer-file-name)
+               (not (equal (vc-state (buffer-file-name)) `up-to-date)))
+    ;; :when active
+    )
 
   (setq powerline-default-separator 'arrow)
 
@@ -42,12 +88,13 @@
       (erc-track :when active)
       ((all-the-icons-vc-icon
         all-the-icons-vc-status
-        :separator "|" nir-git) :when active)
+        nir-git) :when active)
       ((flycheck-error flycheck-warning flycheck-info)
        :when active
        :priority 3)
       (org-pomodoro :when active)
       (org-clock :when active)
+      (all-the-icons-multiple-cursors)
       )
     `
     (
