884ecf07e5bef037382c2daee23c8cf7b5cf430d
Nir Friedman <nir@tower-research.com>
2022-12-27 10:06:21 -0500

    Delete unused eclipse and spacemacs files
    Change-Id: I0dd923315f1ac5fae352e6045a60c07c1ae7c9e5

--- spacemacs.d/layers/terminal/packages.el
+++
@@ -1,6 +0,0 @@
-(setq terminal-packages '(xclip))
-
-(defun terminal/init-xclip ()
-  (use-package xclip
-    :config
-      (xclip-mode 1)))
--- spacemacs.d/layers/worse/packages.el
+++
@@ -1,7 +0,0 @@
-(setq worse-packages
-      '(
-        (worse :location local)))
-
-(defun worse/init-worse ()
-  (use-package worse
-    :init (spacemacs/set-leader-keys "pf" 'worse-faster-projectile-files)))
