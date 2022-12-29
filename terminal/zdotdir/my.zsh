8f9e121252d97afcdb3d591de38c7a6e8492422f
Nir Friedman <nir@tower-research.com>
2022-12-29 13:46:06 -0500

    Add c-e history word search
    Change-Id: I2bcab3f323e5608497f07cfe12147566d6d3b570

--- terminal/zdotdir/my.zsh
+++ terminal/zdotdir/my.zsh
@@ -50,6 +50,8 @@ export FZF_DEFAULT_OPTS="-e \
    --color 16,fg:11,bg:-1,hl:1,hl+:1,bg+:7,fg+:11 \
    --color prompt:4,pointer:13,marker:13,spinner:3,info:3"
 
+
+
 # Simply use C-f to trigger fzf; no trigger + tab
 export FZF_COMPLETION_TRIGGER=''
 bindkey '^F' fzf-completion
@@ -57,6 +59,35 @@ bindkey '^I' $fzf_default_completion
 
 # Use C-g for cd completion as Alt is awkward
 bindkey '^G' fzf-cd-widget
+
+# CTRL-E - word based history search
+__hist_word_sel() {
+
+  local cmd='for line in $(fc -l 0 | cut -d '' '' -f 3-); do for word in $line; do echo $word; done; done | sort --unique'
+  setopt localoptions pipefail no_aliases 2> /dev/null
+  local item
+  eval "$cmd" | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse --bind=ctrl-z:ignore ${FZF_DEFAULT_OPTS-} ${FZF_CTRL_T_OPTS-}" $(__fzfcmd) -m "$@" | while read item; do
+    echo -n "${(q)item} "
+  done
+  local ret=$?
+  echo
+  return $ret
+}
+
+__fzfcmd() {
+  [ -n "${TMUX_PANE-}" ] && { [ "${FZF_TMUX:-0}" != 0 ] || [ -n "${FZF_TMUX_OPTS-}" ]; } &&
+    echo "fzf-tmux ${FZF_TMUX_OPTS:--d${FZF_TMUX_HEIGHT:-40%}} -- " || echo "fzf"
+}
+
+fzf-history-word-widget() {
+  LBUFFER="${LBUFFER}$(__hist_word_sel)"
+  local ret=$?
+  zle reset-prompt
+  return $ret
+}
+zle -N fzf-history-word-widget
+bindkey '^E' fzf-history-word-widget
+
 
 # Going up dirs is more useful than ever, since fzf only searches down!
 bindkey -s '^K' 'cd ..\n'
