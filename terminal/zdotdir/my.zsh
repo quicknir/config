802283e7a0427c5c36e44ae5d86bdfaaa9f94a79
Nir Friedman <nir@tower-research.com>
2022-12-30 19:59:12 -0500

    Improvements to my.zsh; coloring, better movement, source ignore.zsh
    Change-Id: Id6eb27ddb33034e12adca9e8aabba2c7db1dcfd3

--- terminal/zdotdir/my.zsh
+++ terminal/zdotdir/my.zsh
@@ -13,13 +13,51 @@ if [[ -s "${ZDOTDIR:-$HOME}/.zprezto/init.zsh" ]]; then
   source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"
 fi
 
+# Better ls coloring when using solarized terminal theme
+eval $(eval "dircolors ${termdir}/dircolors-solarized/dircolors.ansi-light")
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+
+# Handy reference, courtesy of https://github.com/seebi/dircolors-solarized
+# SOLARIZED HEX     16/8 TERMCOL  XTERM/HEX   L*A*B      sRGB        HSB
+# --------- ------- ---- -------  ----------- ---------- ----------- -----------
+# base03    #002b36  8/4 brblack  234 #1c1c1c 15 -12 -12   0  43  54 193 100  21
+# base02    #073642  0/4 black    235 #262626 20 -12 -12   7  54  66 192  90  26
+# base01    #586e75 10/7 brgreen  240 #4e4e4e 45 -07 -07  88 110 117 194  25  46
+# base00    #657b83 11/7 bryellow 241 #585858 50 -07 -07 101 123 131 195  23  51
+# base0     #839496 12/6 brblue   244 #808080 60 -06 -03 131 148 150 186  13  59
+# base1     #93a1a1 14/4 brcyan   245 #8a8a8a 65 -05 -02 147 161 161 180   9  63
+# base2     #eee8d5  7/7 white    254 #d7d7af 92 -00  10 238 232 213  44  11  93
+# base3     #fdf6e3 15/7 brwhite  230 #ffffd7 97  00  10 253 246 227  44  10  99
+# yellow    #b58900  3/3 yellow   136 #af8700 60  10  65 181 137   0  45 100  71
+# orange    #cb4b16  9/3 brred    166 #d75f00 50  50  55 203  75  22  18  89  80
+# red       #dc322f  1/1 red      160 #d70000 50  65  45 220  50  47   1  79  86
+# magenta   #d33682  5/5 magenta  125 #af005f 50  65 -05 211  54 130 331  74  83
+# violet    #6c71c4 13/5 brmagenta 61 #5f5faf 50  15 -45 108 113 196 237  45  77
+# blue      #268bd2  4/4 blue      33 #0087ff 55 -10 -45  38 139 210 205  82  82
+# cyan      #2aa198  6/6 cyan      37 #00afaf 60 -35 -05  42 161 152 175  74  63
+# green     #859900  2/2 green     64 #5f8700 60 -20  65 133 153   0  68 100  60
+
+# Usage: palette
+palette() {
+    local -a colors
+    for i in {000..16}; do
+        colors+=("%F{$i}hello: $i%f")
+    done
+    print -cP $colors
+}
+
+# Usage: printc COLOR_CODE
+printc() {
+    local color="%F{$1}"
+    echo -E ${(qqqq)${(%)color}}
+}
+
+# Colors for fzf-tab
+zstyle ':fzf-tab:*' default-color $'\033[93m'
+
 # Our zshenv has handled setting zdotdir to the path within our repo, so now
 # we can use zdotdir to locate the rest of our config
 termdir="${ZDOTDIR:h}"
-
-# Better ls coloring when using solarized terminal theme
-eval $(eval "dircolors ${termdir}/dircolors-solarized/dircolors.ansi-light")
-zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
 
 alias vi=vim
 # For better vi usability, reduce key delay/timeout                                                                       KEYTIMEOUT=1
@@ -83,12 +121,56 @@ alias hist-dur='history -iD 0 | fzf'
 # Suffixes!
 alias -s txt=vim
 
+# Named directories
+hash -d config="${ZDOTDIR:h:h}"
+
 # Support for GUI clipboard
 source $ZDOTDIR/clipboard.zsh
 
 # A separate file that gets sourced; convenient for putting things you may not want to upstream
-source $ZDOTDIR/more.zsh
+() { local FILE="$ZDOTDIR/ignore.zsh" && test -f $FILE && . $FILE }
 
+# recent directories
+autoload -Uz chpwd_recent_dirs cdr add-zsh-hook
+add-zsh-hook chpwd chpwd_recent_dirs
+zstyle ':chpwd:*' recent-dirs-max 1000
+
+# Replace the fzf cd widget. Our widget doesn't print the line
+fzf-cd-widget() {
+  local cmd="${FZF_ALT_C_COMMAND:-"command find -L . -mindepth 1 \\( -path '*/\\.*' -o -fstype 'sysfs' -o -fstype 'devfs'     -o -fstype 'devtmpfs' -o -fstype 'proc' \\) -prune \
+    -o -type d -print 2> /dev/null | cut -b3-"}"
+  setopt localoptions pipefail no_aliases 2> /dev/null
+  local dir="$(eval "$cmd" | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse --bind=ctrl-z:ignore ${FZF_DEFAULT_OPTS-} ${FZF_ALT_C_OPTS-}" $(__fzfcmd) +m)"
+  if [[ -z "$dir" ]]; then
+    zle redisplay
+    return 0
+  fi
+  builtin cd -q "${(q)dir}" && my-redraw-prompt;
+  local ret=$?
+  return $ret
+}
+zle -N fzf-cd-widget
+
+
+fzf-recent-dir-widget() {
+  local cmd="cdr -l | tr -s ' ' | cut -d ' ' -f 2-"
+  setopt localoptions pipefail no_aliases 2> /dev/null
+  local dir="$(eval "$cmd" | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse --bind=ctrl-z:ignore ${FZF_DEFAULT_OPTS-} ${FZF_ALT_C_OPTS-}" $(__fzfcmd) +m)"
+  if [[ -z "$dir" ]]; then
+    zle redisplay
+    return 0
+  fi
+  eval "dir=$dir"  # Force ~ expansion
+  builtin cd -q "${dir}" && my-redraw-prompt;
+  local ret=$?
+  return $ret
+}
+zle -N fzf-recent-dir-widget
+bindkey '^F' fzf-recent-dir-widget
+
+# Intuitive back-forward navigation, similar to a browser.
+# Also provides up (cd ..), and down (fzf recursive dir search).
+# Bound to Ctrl-hjkl
 # https://www.reddit.com/r/zsh/comments/ka4sae/navigate_folder_history_like_in_fish/
 function my-redraw-prompt() {
   {
