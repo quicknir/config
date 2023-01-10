e0831ae6adbc86b7e6636516bfb783fd2237dd54
Nir Friedman <quicknir@gmail.com>
2023-01-09 22:04:04 -0500

    Integrate desired parts of some prezto modules inline

--- terminal/zdotdir/my_rc.zsh
+++ terminal/zdotdir/my_rc.zsh
@@ -76,7 +76,9 @@ bindkey -M menuselect '^H' backward-char
 bindkey -M menuselect '^L' forward-char
 
 # Edit command in full blown vim; bound to normal mode C-e
-bindkey -M vicmd "$key_info[Control]E" edit-command-line
+autoload -Uz edit-command-line
+zle -N edit-command-line
+bindkey -M vicmd "^E" edit-command-line
 
 # rg/fzf interactive search widget
 rg_fzf_search_widget() { "${ZDOTDIR}/search.sh" }
@@ -130,7 +132,7 @@ bindkey '^E' fzf-history-word-widget
 alias hist-dur='history -iD 0 | fzf'
 
 # Suffixes!
-alias -s {txt,json}=vim
+alias -s {txt,json,sh,zsh}=vim
 alias -s log=bat
 alias -s git='git clone'
 
@@ -243,6 +245,65 @@ bindkey -v '^J' fzf-cd-widget
 bindkey -v '^T' fzf-file-widget
 
 maybe_source "$ZDOTDIR/ignore_rc.zsh"
+
+# From prezto
+# environment
+
+setopt COMBINING_CHARS      # Combine zero-length punctuation characters (accents)
+                            # with the base character.
+setopt INTERACTIVE_COMMENTS # Enable comments in interactive shell.
+setopt RC_QUOTES            # Allow 'Henry''s Garage' instead of 'Henry'\''s Garage'.
+unsetopt MAIL_WARNING       # Don't print a warning message if a mail file has been accessed.
+
+# Allow mapping Ctrl+S and Ctrl+Q shortcuts
+[[ -r ${TTY:-} && -w ${TTY:-} && $+commands[stty] == 1 ]] && stty -ixon <$TTY >$TTY
+
+#
+# Jobs
+#
+
+setopt LONG_LIST_JOBS     # List jobs in the long format by default.
+setopt AUTO_RESUME        # Attempt to resume existing job before creating a new process.
+setopt NOTIFY             # Report status of background jobs immediately.
+unsetopt BG_NICE          # Don't run all background jobs at a lower priority.
+unsetopt HUP              # Don't kill jobs on shell exit.
+unsetopt CHECK_JOBS       # Don't report on jobs when shell exit.
+
+# history
+
+setopt BANG_HIST                 # Treat the '!' character specially during expansion.
+setopt EXTENDED_HISTORY          # Write the history file in the ':start:elapsed;command' format.
+setopt SHARE_HISTORY             # Share history between all sessions.
+setopt HIST_EXPIRE_DUPS_FIRST    # Expire a duplicate event first when trimming history.
+setopt HIST_IGNORE_DUPS          # Do not record an event that was just recorded again.
+setopt HIST_IGNORE_ALL_DUPS      # Delete an old recorded event if a new event is a duplicate.
+setopt HIST_FIND_NO_DUPS         # Do not display a previously found event.
+setopt HIST_IGNORE_SPACE         # Do not record an event starting with a space.
+setopt HIST_SAVE_NO_DUPS         # Do not write a duplicate event to the history file.
+setopt HIST_VERIFY               # Do not execute immediately upon history expansion.
+setopt HIST_BEEP                 # Beep when accessing non-existent history.
+
+HISTFILE="${ZDOTDIR}/.zsh_history"  # The path to the history file.
+HISTSIZE=10000  # The maximum number of events to save in the internal history.
+SAVEHIST=10000  # The maximum number of events to save in the history file.
+
+
+setopt AUTO_CD              # Auto changes to a directory without typing cd.
+setopt AUTO_PUSHD           # Push the old directory onto the stack on cd.
+setopt PUSHD_SILENT         # Do not print the directory stack after pushd or popd.
+setopt PUSHD_TO_HOME        # Push to home directory when no argument is given.
+setopt CDABLE_VARS          # Change directory to a path stored in a variable.
+setopt MULTIOS              # Write to multiple descriptors.
+setopt EXTENDED_GLOB        # Use extended globbing syntax.
+unsetopt CLOBBER            # Do not overwrite existing files with > and >>.
+                            # Use >! and >>! to bypass.
+
+# utility
+# Safe ops. Ask the user before doing anything destructive.
+alias cp='cp -i'
+alias ln='ln -i'
+alias mv='mv -i'
+alias rm='rm -i'
 
 # To customize prompt, run `p10k configure` or edit $ZDOTDIR/.p10k.zsh.
 [[ ! -f "${ZDOTDIR}/.p10k.zsh" ]] || source "${ZDOTDIR}/.p10k.zsh"
