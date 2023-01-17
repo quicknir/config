2b328731f847ea038c544647543192d325b363ee
Nir Friedman <quicknir@gmail.com>
2023-01-17 13:06:02 -0500

    More fixes and improvements for colored history

--- terminal/zdotdir/my_rc.zsh
+++ terminal/zdotdir/my_rc.zsh
@@ -470,7 +470,7 @@ highlight_to_str() {
     local highlight_arr_name=$2
     local -A index_map
     local str_length=${#str}
-    local i pre_escape post_escape s
+    local i pre_escape post_escape s v
 
     for i in {1..${#str}}; do index_map[$i]=$i; done
 
@@ -479,15 +479,17 @@ highlight_to_str() {
 
         if [[ ${#parts} != 3 ]]; then 
             # Sometimes we get bad responses from fast-syntax highlighting
-            # git commands are the usual culprit it seems
+            # e.g. if fed '($index++)', we get
+            # 0 1 fg=yellow 1 9 fg=red,bold 9 10 fg=yellow 0 1 fg=green,bold 9 10 fg=green,bold 9 10 bg=blue  1 bg=blue
+            # That trailing style doesn't have a begin and end; so we just ignore it
             continue
         fi
 
         local first=$((parts[1]+1))
         local last=$parts[2]
 
-        if [[ $first > $last ]]; then
-            # Again, fast-syntax sometimes includes segments like this;
+        if [[ $first -ge $last ]]; then
+            # Again, fast-syntax sometimes includes segments like this where start is greater than end
             # have observed it with git commit -m <quoted text>
             continue
         fi
@@ -499,40 +501,58 @@ highlight_to_str() {
         apply_format_to_substr $str
         str=${(%)s}
         for i in {$first..$last}; do
-            local v=$index_map[$i]
+            v=$index_map[$i]
             index_map[$i]=$((v+pre_escape_len))
         done
-        for i in {$last..$str_length}; do
-            local v=$index_map[$i]
-            index_map[$i]=$((v+pre_escape_len+post_escape_len))
-        done
+        if [[ $last != $str_length ]]; then
+            for i in {$((last+1))..$str_length}; do
+                v=$index_map[$i]
+                index_map[$i]=$((v+pre_escape_len+post_escape_len))
+            done
+        fi
     done
     echo -n $str
 }
 
+# the setup of fast-syntax-highlight calls was taken from here
+# https://github.com/zdharma-continuum/fast-syntax-highlighting/blob/master/test/parse.zsh#L180
+--fast-syntax-highlight-str() {
+    emulate -L zsh
+    reply=()
+    -fast-highlight-init
+
+    local right_brace_is_recognized_everywhere
+    integer path_dirs_was_set multi_func_def ointeractive_comments
+    -fast-highlight-fill-option-variables
+
+    -fast-highlight-process "" "$1" "0"
+    -fast-highlight-string-process "" "$1"
+
+    highlight_to_str $1 reply
+}
+
 make_hist_color_file() {
-    # some kind of insane caching mechanism that produces garbage for the
-    # first attempt to syntax highlight git
-    -fast-highlight-process "" 'git push' 0
+    emulate -L zsh
+    # sometimes get some crazy git dump the first time we syntax highlight some git stuff
+    -fast-highlight-process "" 'git diff foo' "0"
+
     rm -f "${HISTFILE}.color"
+
     local line
     for line in ${(Oa)history[@]}; do
-        local reply=()
-        -fast-highlight-process "" "$line" 0
-        -fast-highlight-string-process "" "$line" 0
-        highlight_to_str "$line" reply >>! "${HISTFILE}.color"
+        --fast-syntax-highlight-str $line >>! "${HISTFILE}.color"
         echo '' >>! "${HISTFILE}.color"
     done
 }
 
-add_color_history_entry() {
+--add-color-history-entry() {
+    emulate -L zsh
     if [[ ${#1} > 1 ]]; then
-        local reply=()
-        -fast-highlight-process "" "$1" 0
-        -fast-highlight-string-process "" "$1" 0
-        highlight_to_str "$1" reply >>! "${HISTFILE}.color"
+        # sometimes get some crazy git dump the first time we syntax highlight some git stuff
+        -fast-highlight-process "" 'git diff foo' "0"
+        --fast-syntax-highlight-str $1 >>! "${HISTFILE}.color"
     fi
 }
 
 autoload -Uz add-zsh-hook
-add-zsh-hook zshaddhistory add_color_history_entry
+add-zsh-hook zshaddhistory --add-color-history-entry
