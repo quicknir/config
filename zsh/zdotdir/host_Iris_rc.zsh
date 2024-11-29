882f162efe1c688c8c79d0e5f477161e17b038ec
Nir Friedman <quicknir@gmail.com>
2024-11-29 16:47:57 -0500

    On Iris, error silently if main env not available

--- zsh/zdotdir/host_Iris_rc.zsh
+++ zsh/zdotdir/host_Iris_rc.zsh
@@ -1,3 +1,4 @@
 export MAMBA_ROOT_PREFIX=/home/nir/micromamba
 eval "$(~nikud/micromamba/bin/micromamba shell hook --shell zsh)" 
-micromamba activate main
+# Try activating main; if not available error silently
+micromamba activate main 2> /dev/null
